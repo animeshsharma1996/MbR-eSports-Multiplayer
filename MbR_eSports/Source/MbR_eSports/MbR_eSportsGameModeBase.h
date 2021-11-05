5c1051427ff9ffd957a012cfc1cdf01cbc2ce1d3
Animesh Sharma <animeshsharma1996@gmail.com>
2021-11-05 12:42:14 +0000

    Unreal Engine Project Init with new git-ignore

---
+++ MbR_eSports/Source/MbR_eSports/MbR_eSportsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MbR_eSportsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MBR_ESPORTS_API AMbR_eSportsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
