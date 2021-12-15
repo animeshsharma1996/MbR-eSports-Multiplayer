20060c7868f3c97ab683dea9e9759712dbea2784
Animesh Sharma <animeshsharma1996@gmail.com>
2021-12-15 03:15:39 +0000

    Online Subsystem Dependency in Build file

--- MbR_eSports/Source/MbR_eSports/MbR_eSports.Build.cs
+++ MbR_eSports/Source/MbR_eSports/MbR_eSports.Build.cs
@@ -13,11 +13,9 @@ public class MbR_eSports : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		// Uncomment if you are using online features
+		PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemSteam" });
 	}
 }
